2a20e500fc43dd9388ab847ad340250bf5190d59
MuhammadaminAbbosov <musulmaanjanob@gmail.com>
2022-01-15 00:33:07 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,39 @@
+const count = document.getElementById("count")
+const clear = document.getElementById("clear")
+
+let s = 0
+
+
+count.addEventListener("click",function(e) {
+    e.preventDefault()
+    s++
+    document.getElementById("counter").textContent = s
+
+    if(Number(s) % 33 === 0 && Number(s) % 9 === 0)
+    {
+        document.querySelector(".circle").style.border = "5px solid yellow"
+        document.querySelector(".word").textContent = "Subhanalloh"
+        document.querySelector(".arab").textContent = "سبحان الله"
+    }        
+    else if(Number(s) % 33 === 0 && Number(s) % 6 === 0)
+    {
+        document.querySelector(".circle").style.border = "5px solid red"
+        document.querySelector(".word").textContent = "Allohu akbar"
+        document.querySelector(".arab").textContent = " الله أكبر"
+    }        
+    else if(Number(s) % 33 === 0 && Number(s) % 3 === 0)
+    {
+        document.querySelector(".circle").style.border = "5px solid blue"
+        document.querySelector(".word").textContent = "Alhamdulillah"
+        document.querySelector(".arab").textContent = " ٱلْحَمْدُ لِلَّٰهِ"
+    }        
+})
+
+clear.addEventListener("click",function(e) {
+    e.preventDefault()
+    s = 0
+    document.getElementById("counter").textContent = s
+
+    if(Number(s) % 33 === 0)
+        document.querySelector(".circle").style.border = "5px solid rgb(23, 247, 23)"
+})
